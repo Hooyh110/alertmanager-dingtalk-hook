d99550577b60086a7d431c76570b5dbbc3e7089b
YP <389736180@qq.com>
2019-07-15 14:29:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
 import requests
+import arrow
 
 from flask import Flask
 from flask import request
@@ -29,16 +30,35 @@ def send_alert(data):
         print('you must set ROBOT_TOKEN env')
         return
     url = 'https://oapi.dingtalk.com/robot/send?access_token=%s' % token
-    send_data = {
-        "msgtype": "text",
-        "text": {
-            "content": data
+    for output in data['alerts'][:]:
+        try:
+            pod_name = output['labels']['pod_name']
+            namespace = output['labels']['namespace']
+            message = output['annotations']['message']
+        except KeyError:
+            pod_name = 'null'
+            namespace = 'null'
+            message = output['annotations']['description']
+
+        send_data = {
+            "msgtype": "markdown",
+            "markdown": {
+                "title": "prometheus_alert",
+                "text": "## 告警程序: prometheus_alert \n" +
+                        "**告警级别**: %s \n\n" % output['labels']['severity'] +
+                        "**告警类型**: %s \n\n" % output['labels']['alertname'] +
+                        "**故障pod**: %s \n\n" % pod_name +
+                        "**故障namespace**: %s \n\n" % namespace +
+                        "**告警详情**: %s \n\n" % message +
+                        "**告警状态**: %s \n\n" % output['status'] +
+                        "**触发时间**: %s \n\n" % arrow.get(output['startsAt']).to('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss ZZ') +
+                        "**触发结束时间**: %s \n" % arrow.get(output['endsAt']).to('Asia/Shanghai').format('YYYY-MM-DD HH:mm:ss ZZ')
+            }
         }
-    }
-    req = requests.post(url, json=send_data)
-    result = req.json()
-    if result['errcode'] != 0:
-        print('notify dingtalk error: %s' % result['errcode'])
+        req = requests.post(url, json=send_data)
+        result = req.json()
+        if result['errcode'] != 0:
+            print('notify dingtalk error: %s' % result['errcode'])
 
 
 if __name__ == '__main__':
